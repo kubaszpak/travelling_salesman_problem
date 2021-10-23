4b156d4925a9fbd77ebec731deb345a437631967
kubaszpak <jakub.szpak.it@gmail.com>
2021-10-23 21:39:16 +0200

    Added timing

--- pea.cpp
+++ pea.cpp
@@ -1,18 +1,26 @@
 #include "AdjencyMatrix.h"
+#include <chrono>
 
 using namespace std;
 
 int main()
 {
 
-    std::string file_name = "instances/m6.atsp";
+    std::string file_name = "instances/m11.atsp";
     AdjencyMatrix matrix = AdjencyMatrix(file_name);
     matrix.print();
 
     // cout << "Pierwszy rząd, pierwsza kolumna: " << matrix.get(0, 0) << endl;
-    cout << "Wartosc optymalna: " << matrix.OPT << endl;
+    // cout << "Wartosc optymalna: " << matrix.OPT << endl;
 
+    // Pomiar czasu
+
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
     matrix.brute_force_tsp();
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
+    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[µs]" << std::endl;
+    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count() << "[ns]" << std::endl;
 
     return 0;
 }
